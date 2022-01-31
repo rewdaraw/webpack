8d732a15c47c6819240a7821399b14a8074f1223
RT <rewdaraw@gmail.com>
2022-01-31 14:58:04 +0000

    section: 4-13 configure babel for react

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,18 @@ module.exports = {
           },
         ],
       },
+
+      // babel
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
     ],
   },
   devServer: {
